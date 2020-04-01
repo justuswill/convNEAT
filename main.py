7331d9e4d5f6cbc408b75e4737cc90558eb3c03a
will <Kathedrale-Giraffe>
2020-04-01 11:50:15 +0200

    Bug added

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ if __name__ == '__main__':
     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     data_train, data_test = mnist(torch_device)
 
-    trainer = ConvNEAT(output_size=10, n=15, torch_device=torch_device, name='debug_run_10', seed=9)
-    trainer.prompt(data_train, save_mode="elites", elitism_rate=0.3, min_species_size=4, epochs=1,
-                   n_generations_no_change=3, tol=0, min_species=1, mutate_speed=0.2)
+    trainer = ConvNEAT(output_size=10, n=100, torch_device=torch_device, name='test_run_3', seed=10)
+    trainer.prompt(data_train, save_mode="elites", elitism_rate=0.3, min_species_size=4, epochs=3,
+                   n_generations_no_change=3, tol=0, min_species=2, mutate_speed=0.3)
