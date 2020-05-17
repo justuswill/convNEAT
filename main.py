8ee04c09bb58ff336e6f919283d0ccfe8fd4e3b7
Justus <justuswill@gmail.com>
2020-05-17 22:07:11 +0200

    Teil der Grafiken eingefügt

--- main.py
+++ main.py
@@ -25,10 +25,14 @@ def mnist(torch_device):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level='INFO')  
+    # Debugging
+    logging.basicConfig(level='INFO')
+    # Train on GPU
     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # The dataset
     data_train, data_test = mnist(torch_device)
 
-    trainer = ConvNEAT(output_size=10, n=10, torch_device=torch_device, name='test_5', seed=12)
-    trainer.prompt(data_train, save_mode="elites", elitism_rate=0.2, min_species_size=4, epochs=3,
-                   n_generations_no_change=3, tol=0, min_species=1, mutate_speed=0.3)
+    # train
+    trainer = ConvNEAT(output_size=10, n=20, torch_device=torch_device, name='test_run_1', seed=15)
+    trainer.prompt(data_train, save_mode="elites", elitism_rate=0.3, min_species_size=4, epochs=2,
+                   n_generations_no_change=3, tol=0, min_species=2, mutate_speed=0.3)
